d6e71605a226f5987f9b7635220a237d9e22ef55
Fox Liu <lgxxabc@gmail.com>
2021-08-12 22:25:44 -0500

    Correct the TC

--- TimeComplexity3.java
+++ TimeComplexity3.java
@@ -10,5 +10,9 @@ public class TimeComplexity3 {
 	}
 
 //	Solution:
-//	Time complexity TC = O(N)
+//	Time complexity TC = O(logN)
+//	The for loop says for(int i = 0, i < N, i *= 2) -- it isn't incrementing i normally, 
+//	instead it doubles it with each iteration. 
+//	That means that the loop will iterate fewer times, and leads to a time complexity of log base 2 of N, 
+//	or lb(N), which is actually more efficient than O(N).
 }
